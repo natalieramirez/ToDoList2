d96f532613f847e67980a874b35ab08e59917278
Natalie Ramirez <60053094+natalieramirez@users.noreply.github.com>
2020-06-23 10:44:03 -0400

    added label to storyboard

---
+++ ToDoList2/AddToDoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddToDoViewController.swift
+//  ToDoList2
+//
+//  Created by Natalie Ramirez on 6/22/20.
+//  Copyright © 2020 Natalie Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoViewController: UIViewController {
+
+    var previousVC = TableViewController()
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = ToDo()
+
+        if let titleText = titleTextField.text {
+          toDo.name = titleText
+          toDo.important = importantSwitch.isOn
+        }
+        previousVC.toDos.append(toDo)
+        previousVC.tableView.reloadData()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
